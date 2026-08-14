6167ab6586814f9764759c584897b73edc7e3b9c
Max <61809127+maxwellpark@users.noreply.github.com>
2026-08-14 07:54:01 +0100

    Drop LINQ and sqrt from the per-frame closest-piece lookup

--- Assets/Scripts/Common/Utils.cs
+++ Assets/Scripts/Common/Utils.cs
@@ -1,16 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public static class Utils
 {
-    public static bool GetAnyKeyDown(IEnumerable<KeyCode> keys)
-    {
-        return keys.Any(k => Input.GetKeyDown(k));
-    }
-
     public static bool ApproximatelyEquals(this Vector3 self, Vector3 other, float tolerance = 0.001f)
     {
-        return Vector3.Distance(self, other) <= tolerance;
+        return (self - other).sqrMagnitude <= tolerance * tolerance;
     }
 }
